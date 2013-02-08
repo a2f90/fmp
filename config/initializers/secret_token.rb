2a1ad13f93132b634da58bccbdea9142efb5652c
Andy Farmer <andyfarmer@Andys-MacBook-Air.local>
2013-02-08 16:02:55 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmp::Application.config.secret_token = '01bb435eb02501dd047bc4f9f2fc40325e3a5c2092cf232a0f2349384e872622d1aabfdb60b27bf099698661bb8f79f07ea126839301263473fb6c5b5ebd9ada'
